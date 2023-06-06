e1914d358a70f9ed3e328cc219a037e4cb9e1f62
Aluno <marianathayza3@gmail.com>
2023-06-06 17:24:33 -0300

    06/06/2023

--- app/src/main/java/com/example/minhasmolduras/MainActivity.kt
+++ app/src/main/java/com/example/minhasmolduras/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.minhasmolduras
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.minhasmolduras.ui.theme.MinhasMoldurasTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +40,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MinhasMoldurasTheme {
-        Greeting("Android")
+fun MolduraImagem(
+    idImagem: Int,
+    idDescricao: Int
+)
+
+{
+    Card() {
+    Column() {
+        Image(
+            painter = painterResource(id = idImagem),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(400.dp)
+
+        )
+      Text(
+          text = stringResource(idDescricao),
+          fontSize = 25.sp,
+          fontFamily = FontFamily.Cursive,
+          textAlign = TextAlign.Center,
+          fontWeight = FontWeight.Black,
+          modifier = Modifier
+              .fillMaxSize()
+
+      )
+
     }
+   }
+ }
+@Preview
+@Composable
+fun PreviewMoldura(){
+
+    MolduraImagem(
+        idImagem = R.drawable.barbuleta1,
+        idDescricao = R.string.descricao_imagem1)
 }
